a2297820a6dc12a6c2ba7b5720097db5c0f64f26
Tobias Schwab <tobias.schwab@dynport.de>
2015-08-29 23:31:02 +0200

    add version and index links

--- gen.go
+++ gen.go
@@ -3,10 +3,13 @@ package main
 import (
 	"bytes"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/shurcooL/github_flavored_markdown"
 )
 
 func main() {
@@ -17,45 +20,80 @@ func main() {
 }
 
 type Job struct {
-	Out   string
-	Parts []string
+	Out     string
+	Sources []Source
 }
 
 func (j *Job) Exec(l Logger) error {
-	return writeFile(l, j.Out, j.Parts...)
+	return writeFile(l, j.Out, j.Sources...)
+}
+
+func FileSources(paths ...string) (list []Source) {
+	for _, p := range paths {
+		list = append(list, FileSource(p))
+	}
+	return list
+
+}
+
+var sources = map[string][]Source{
+	"dotfiles.html":       Layout(FileSource("tpl/dotfiles/index.tpl")),
+	"dotfiles/vimrc.conf": FileSources("dotfiles/vimrc"),
+	"index.html":          Layout(FileSource("tpl/index.tpl")),
+	"versions.html":       Layout(MarkdownSource("tpl/versions.md")),
+}
+
+func Layout(s Source) []Source {
+	return []Source{FileSource("tpl/header.tpl"), s, FileSource("tpl/footer.tpl")}
+}
+
+func MarkdownSource(path string) Source {
+	return func(w io.Writer) error {
+		b, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		b = github_flavored_markdown.Markdown(b)
+		_, err = w.Write(b)
+		return err
+	}
 }
 
 func run(l *log.Logger) error {
-	jobs := []*Job{
-		{"index.html", []string{"tpl/header.tpl", "tpl/index.tpl", "tpl/footer.tpl"}},
-		{"dotfiles/index.html", []string{"tpl/header.tpl", "tpl/dotfiles/index.tpl", "tpl/footer.tpl"}},
-		{"dotfiles/vimrc.conf", []string{"dotfiles/vimrc"}},
-	}
-	for _, j := range jobs {
-		if err := j.Exec(l); err != nil {
+	github_flavored_markdown.Markdown([]byte{})
+	for path, sources := range sources {
+		if err := writeFile(l, path, sources...); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
+func FileSource(path string) Source {
+	return func(w io.Writer) error {
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		_, err = io.Copy(w, f)
+		return err
+	}
+}
+
+type Source func(w io.Writer) error
+
 type Logger interface {
 	Printf(string, ...interface{})
 }
 
-func writeFile(l Logger, out string, files ...string) error {
+func writeFile(l Logger, out string, sources ...Source) error {
 	start := time.Now()
 	buf := &bytes.Buffer{}
-	for _, name := range files {
-		f, err := os.Open(name)
-		if err != nil {
+	for _, src := range sources {
+		if err := src(buf); err != nil {
 			return err
 		}
-		_, err = io.Copy(buf, f)
-		if err != nil {
-			return err
-		}
-		f.Close()
 	}
 	tmp := out + ".tmp." + strconv.FormatInt(time.Now().UTC().UnixNano(), 36)
 	f, err := os.Create(tmp)
---
+++ tpl/versions.md
@@ -0,0 +1,28 @@
+## Versions
+
+### original
+
+- [Consul](https://www.consul.io/downloads.html)
+- [CoreOS](https://coreos.com/releases/)
+- [Docker](https://github.com/docker/docker/blob/master/CHANGELOG.md)
+- [ElasticSearch](https://www.elastic.co/downloads/elasticsearch)
+- [Geminabox](https://github.com/geminabox/geminabox/releases)
+- [Golang](https://golang.org/dl/)
+- [Jenkins](https://jenkins-ci.org/changelog)
+- [Kibana](https://www.elastic.co/downloads/kibana)
+- [MySQL](http://dev.mysql.com/downloads/mysql/)
+- [Nginx](http://nginx.org/en/download.html)
+- [PostgreSQL](http://www.postgresql.org/ftp/source/)
+- [Redis](http://redis.io/download)
+- [Ruby](https://www.ruby-lang.org/en/downloads/)
+
+### dockerhub
+
+- [ElasticSearch](https://registry.hub.docker.com/_/elasticsearch/)
+- [Java](https://registry.hub.docker.com/_/java/)
+- [Mongo](https://registry.hub.docker.com/_/mongo/)
+- [MySQL](https://registry.hub.docker.com/_/mysql/)
+- [PHP](https://registry.hub.docker.com/_/php/)
+- [PostgreSQL](https://registry.hub.docker.com/_/postgres/)
+- [Redis](https://registry.hub.docker.com/_/redis/)
+- [Ruby](https://registry.hub.docker.com/_/ruby/)
