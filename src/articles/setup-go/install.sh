dfdd58e40baf6111b62d15467b3c296caeb904e0
Tobias Schwab <tobias.schwab@dynport.de>
2015-09-17 23:31:48 +0200

    set correct mode for go dir

--- src/articles/setup-go/install.sh
+++ src/articles/setup-go/install.sh
@@ -9,6 +9,7 @@ TMP=$(mktemp -d /tmp/goXXXXXX)
 cd ${TMP} && curl -SfL https://storage.googleapis.com/golang/go${VERSION}.${ARCH}.tar.gz | tar xfz - --strip=1
 
 sudo mv ${TMP} ${DST}
+sudo chmod 0755 ${DST}
 sudo ln -nfs ${DST} /usr/local/go
 
 # it is also helpful to delete $GOPATH/pkg (at least when upgrading go < 1.5)
