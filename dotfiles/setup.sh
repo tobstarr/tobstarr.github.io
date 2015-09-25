5a32d2687faefe789b1195f20bb02f2715b0715a
Tobias Schwab <tobias.schwab@dynport.de>
2015-09-25 14:22:52 +0200

    disable writing profile

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -27,9 +27,10 @@ ln -nfs $CONF_DIR/dotfiles/vim-snippets $HOME/.vim/UltiSnips
 ln -nfs $CONF_DIR/dotfiles/tmux.conf $HOME/.tmux.conf
 ln -nfs $CONF_DIR/dotfiles/gitconfig $HOME/.gitconfig
 
-cat <<EOF > $HOME/.profile
-export GOROOT=/usr/local/go
-export GOPATH=$HOME
-export PATH=$GOROOT/bin:$GOPATH/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
-alias vi=$(which vim)
-EOF
+
+# cat <<EOF > $HOME/.profile
+# export 
+# export GOPATH=$HOME
+# export PATH=$GOROOT/bin:$GOPATH/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+# alias vi=$(which vim)
+# EOF
