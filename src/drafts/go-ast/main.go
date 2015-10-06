4ee539912e8d383a63ebc33cf5d58c7db4c007ab
Tobias Schwab <tobias.schwab@dynport.de>
2015-10-06 21:55:06 +0200

    fix ast

--- src/drafts/go-ast/main.go
+++ src/drafts/go-ast/main.go
@@ -1,22 +1,22 @@
-package main
+package ast
 
 import (
 	"go/parser"
 	"go/token"
-	"log"
+	"os"
 )
-
-func main() {
-	if err := run(); err != nil {
-		log.Fatal(err)
-	}
-}
 
 func run() error {
 	fs := token.NewFileSet()
-	res, err := parser.ParseDir(fs, d, nil, parser.ImportsOnly)
+	wd, err := os.Getwd()
 	if err != nil {
 		return err
 	}
+
+	res, err := parser.ParseDir(fs, wd, nil, parser.ImportsOnly)
+	if err != nil {
+		return err
+	}
+	_ = res
 	return nil
 }
