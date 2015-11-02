befe80453a47c453001036e8235bdb110341f195
Tobias Schwab <tobias.schwab@dynport.de>
2015-11-02 08:11:02 +0100

    generate systemd and systemd timer

--- cmd/ts-gen-tobstarr/main.go
+++ cmd/ts-gen-tobstarr/main.go
@@ -35,6 +35,8 @@ var sources = map[string][]Source{
 	"tobstarr.gpg":                         FileSources("src/tobstarr.gpg"),
 	"versions.html":                        Layout(MarkdownSource(FileSource("src/versions.md"))),
 	"zsh.html":                             Layout(MarkdownSource(Render(FileSource("src/zsh/index.md")))),
+	"systemd.html":                         Layout(MarkdownSource(Render(FileSource("src/systemd/index.md")))),
+	"systemd_timer.html":                   Layout(MarkdownSource(Render(FileSource("src/systemd/timers.md")))),
 }
 
 func chain(s string, funcs ...func(Source) Source) Source {
