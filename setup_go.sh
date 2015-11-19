94da404c56ba0d64c110be3d65cdb1aaa5d6c2db
Tobias Schwab <tobias.schwab@dynport.de>
2015-11-19 08:19:00 +0100

    fix setup_go and setup_vim

--- setup_go.sh
+++ setup_go.sh
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>tobstarr.com</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css" media="screen" title="no title" charset="utf-8">
-    <link rel="stylesheet" href="/css/default.css" type="text/css" media="screen" title="no title" charset="utf-8">
-</head>
-<body>
-  <div class="container">
-    <div class="header index_link">
-      <a href="/index.html">tobstarr.com</a>
-    </div>
-    <div class="header_links">
-      <a href="/dotfiles.html">Dotfiles</a>
-      |
-      <a href="/cheats.html">Cheats</a>
-    </div>
-    <hr />
 #!/bin/sh
 set -xe
 
@@ -45,6 +25,3 @@ sudo ln -nfs ${DST} /usr/local/go
 
 # it is also helpful to delete $GOPATH/pkg (at least when upgrading go < 1.5)
 # also make sure to add /usr/local/go/bin to your PATH
-</div>
-</body>
-</html>
--- setup_vim.sh
+++ setup_vim.sh
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>tobstarr.com</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css" media="screen" title="no title" charset="utf-8">
-    <link rel="stylesheet" href="/css/default.css" type="text/css" media="screen" title="no title" charset="utf-8">
-</head>
-<body>
-  <div class="container">
-    <div class="header index_link">
-      <a href="/index.html">tobstarr.com</a>
-    </div>
-    <div class="header_links">
-      <a href="/dotfiles.html">Dotfiles</a>
-      |
-      <a href="/cheats.html">Cheats</a>
-    </div>
-    <hr />
 #!/bin/bash
 set -xe
 
@@ -62,6 +42,3 @@ fi
 
 # /usr/local/bin/vim +BundleInstall +qall > /dev/null 2>&1
 # /usr/local/bin/vim +GoInstallBinaries +qall 2>&1
-</div>
-</body>
-</html>
