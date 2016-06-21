2838e0b6613b8adda56a7752c2027ba3f7feefc7
Tobias Schwab <tobias.schwab@dynport.de>
2016-06-21 09:02:56 +0200

    document how to install nvim on fedora

--- src/neovim/install.sh
+++ src/neovim/install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -xe
 
+## Ubuntu
 apt-get install software-properties-common
 add-apt-repository ppa:neovim-ppa/unstable -y
 apt-get update -y
 apt-get install neovim python-dev python-pip python3-dev python3-pip -y
+pip install -y neovim
+
+## Fedora
+dnf -y install dnf-plugins-core
+dnf -y copr enable dperson/neovim
+dnf -y install neovim
