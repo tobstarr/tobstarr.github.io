338c89aa862a0780afa4e55598ce40fb377882dc
Tobias Schwab <tobias.schwab@dynport.de>
2016-07-05 16:23:49 +0200

    neovim: fix fedora dependencies

--- src/neovim/install.sh
+++ src/neovim/install.sh
@@ -6,9 +6,11 @@ apt-get install software-properties-common
 add-apt-repository ppa:neovim-ppa/unstable -y
 apt-get update -y
 apt-get install neovim python-dev python-pip python3-dev python3-pip -y
-pip install -y neovim
+pip install neovim
 
 ## Fedora
-dnf -y install dnf-plugins-core
+dnf -y install dnf-plugins-core redhat-rpm-config python-devel
+dnf -y group install "C Development Tools and Libraries"
 dnf -y copr enable dperson/neovim
 dnf -y install neovim
+pip install neovim
