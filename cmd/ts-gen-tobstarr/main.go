28f1c16a3fc8a92a1da054244802025bbbc460b9
Tobias Schwab <tobias.schwab@dynport.de>
2016-08-10 08:56:13 +0200

    no longer write CNAME

--- cmd/ts-gen-tobstarr/main.go
+++ cmd/ts-gen-tobstarr/main.go
@@ -20,7 +20,6 @@ import (
 )
 
 var sources = map[string][]Source{
-	"CNAME":                                FileSources("src/CNAME"),
 	"aws-sdk-go.html":                      Layout(MarkdownSource(Render(FileSource("src/aws-sdk-go/index.md")))),
 	"aws.html":                             Layout(MarkdownSource(Render(FileSource("src/aws/index.md")))),
 	"cheats.html":                          Layout(FileSource("src/cheats.tpl")),
